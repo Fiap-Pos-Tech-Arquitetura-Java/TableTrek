6b54c127b4e36d33067bd5cee9be2bb5160c1b7b
Anderson <anderson.wagner@gmail.com>
2024-02-13 19:36:28 -0300

    implementando os testes integrados para o RestauranteService da operações que ainda não haviam sido feitas no criação do RestauranteServiceIT.java

--- src/test/java/br/com/fiap/postech/tabletrek/services/RestauranteServiceIT.java
+++ src/test/java/br/com/fiap/postech/tabletrek/services/RestauranteServiceIT.java
@@ -1,6 +1,7 @@
 package br.com.fiap.postech.tabletrek.services;
 
 
+import br.com.fiap.postech.tabletrek.controller.exception.ControllerNotFoundException;
 import br.com.fiap.postech.tabletrek.dto.RestauranteDTO;
 import br.com.fiap.postech.tabletrek.helper.RestauranteHelper;
 import br.com.fiap.postech.tabletrek.repository.RestauranteRepository;
@@ -9,11 +10,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -54,31 +61,74 @@ class RestauranteServiceIT {
 
     @Test
     void deveGerarExcecao_QuandoBuscarRestaurantePorId_idNaoExiste() {
-        fail("não fiz ainda");
+        // Arrange
+        var restauranteDTO = RestauranteHelper.getRestauranteDTO(true);
+        UUID uuid = restauranteDTO.id();
+        // Act &&  Assert
+        assertThatThrownBy(() -> restauranteService.findById(uuid))
+                .isInstanceOf(ControllerNotFoundException.class)
+                .hasMessage("Restaurante não encontrado com o ID: " + restauranteDTO.id());
     }
 
     @Test
     void devePermitirBuscarTodosRestaurante() {
-        fail("não fiz ainda");
+        // Arrange
+        // Act
+        var listaRestaurantesObtidos = restauranteService.findAll(Pageable.unpaged());
+        // Assert
+        assertThat(listaRestaurantesObtidos).isNotNull().isInstanceOf(Page.class);
+        assertThat(listaRestaurantesObtidos.getContent()).asList().hasSize(3);
+        assertThat(listaRestaurantesObtidos.getContent()).asList().allSatisfy(
+            restauranteObtido -> {
+                assertThat(restauranteObtido).isNotNull();
+            }
+        );
     }
 
     @Test
     void devePermitirAlterarRestaurante() {
-        fail("não fiz ainda");
+        // Arrange
+        var id = UUID.fromString("b35d3a29-408a-4d1a-964c-2261cb0e252f");
+        var nome = "Tojiro Sushi novo";
+        // Act
+        var restauranteAtualizada = restauranteService.update(id, new RestauranteDTO(id, nome));
+        // Assert
+        assertThat(restauranteAtualizada).isNotNull().isInstanceOf(RestauranteDTO.class);
+        assertThat(restauranteAtualizada.id()).isNull();
+        assertThat(restauranteAtualizada.nome()).isEqualTo(nome);
     }
 
     @Test
     void deveGerarExcecao_QuandoAlterarRestaurantePorId_idNaoExiste() {
-        fail("não fiz ainda");
+        // Arrange
+        var restauranteDTO = RestauranteHelper.getRestauranteDTO(true);
+        var uuid = restauranteDTO.id();
+        // Act &&  Assert
+        assertThatThrownBy(() -> restauranteService.update(uuid, restauranteDTO))
+                .isInstanceOf(ControllerNotFoundException.class)
+                .hasMessage("Restaurante não encontrado com o ID: " + restauranteDTO.id());;
     }
 
     @Test
     void devePermitirRemoverRestaurante() {
-        fail("não fiz ainda");
+        // Arrange
+        var id = UUID.fromString("b35d3a29-408a-4d1a-964c-2261cb0e252f");
+        // Act
+        restauranteService.delete(id);
+        // Assert
+        assertThatThrownBy(() -> restauranteService.findById(id))
+                .isInstanceOf(ControllerNotFoundException.class)
+                .hasMessage("Restaurante não encontrado com o ID: " + id);;
     }
 
     @Test
     void deveGerarExcecao_QuandRemoverRestaurantePorId_idNaoExiste() {
-        fail("não fiz ainda");
+        // Arrange
+        var restauranteDTO = RestauranteHelper.getRestauranteDTO(true);
+        var uuid = restauranteDTO.id();
+        // Act &&  Assert
+        assertThatThrownBy(() -> restauranteService.delete(uuid))
+                .isInstanceOf(ControllerNotFoundException.class)
+                .hasMessage("Restaurante não encontrado com o ID: " + restauranteDTO.id());;
     }
 }
